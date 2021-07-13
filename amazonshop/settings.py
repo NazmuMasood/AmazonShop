e4254ea258c1ef9c39693fea4d5f6f7363aeac9c
Nazmu Masood <nazmumasood96@gmail.com>
2021-07-13 10:19:43 +0600

    static_root added in settings.py

--- amazonshop/settings.py
+++ amazonshop/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'amazonshop.herokuapp.com',
+    'amazonshopping.herokuapp.com',
 ]
 
 
@@ -131,3 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
